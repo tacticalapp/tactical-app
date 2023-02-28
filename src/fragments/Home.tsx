4008b94bc9c061955c88f22f23954d6da677658e
Steve Korshakov <steve@korshakov.com>
2023-02-28 23:11:36 +0400

    ref: add theme

--- src/fragments/Home.tsx
+++ src/fragments/Home.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Text } from '../components/Themed';
 
 export const Home = React.memo(() => {
     return (
         <View style={{ flexGrow: 1, flexDirection: 'column', alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ color: 'white' }}>Hello</Text>
+            <Text style={{ fontSize: 32, color: 'white' }}>Hello</Text>
         </View>
     );
 });
