84efe0583d765f547a9e0430b87ee5031d3e1fc2
Steve Korshakov <steve@korshakov.com>
2023-02-28 23:31:57 +0400

    ref: redesign home

--- src/fragments/Home.tsx
+++ src/fragments/Home.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 import { Text } from '../components/Themed';
+import logo from '../assets/logo.svg';
 
 export const Home = React.memo(() => {
     return (
         <View style={{ flexGrow: 1, flexDirection: 'column', alignSelf: 'stretch', alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 32, color: 'white' }}>Hello</Text>
+            <Image source={{ uri: logo }} style={{ width: 84, height: 84, marginBottom: 32 }} />
+            <Text style={{ fontSize: 24, opacity: 0.8, fontWeight: '500' }}>Log in to Tactical</Text>
         </View>
     );
 });
