7f3fe00acc40fb277168d1af2d01441d880f60f3
Steve Korshakov <steve@korshakov.com>
2023-03-13 14:03:05 +0000

    wip: working on electron app

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Boot } from './boot';
-import './main.css';
+import './renderer.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
