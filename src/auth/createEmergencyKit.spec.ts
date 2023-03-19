118f0c7ea081a2f0218dfcfaf73a772649796641
Steve Korshakov <steve@korshakov.com>
2023-03-19 00:16:40 +0000

    ref: comment out pdf test

--- src/auth/createEmergencyKit.spec.ts
+++ src/auth/createEmergencyKit.spec.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 
 describe('createEmergencyKit', () => {
     it('should create an emergency kit', async () => {
-        const kit = await createEmergencyKit({ username: 'steve', secretKey: '12312312312312312' });
-        fs.writeFileSync(__dirname + '/createEmergencyKit.pdf', kit);
+        // const kit = await createEmergencyKit({ username: 'steve', secretKey: '12312312312312312' });
+        // fs.writeFileSync(__dirname + '/createEmergencyKit.pdf', kit);
     });
 });
