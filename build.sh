7e150ca850dad01efc5b23b12e9bf3ff98e8650d
Steve Korshakov <steve@korshakov.com>
2023-03-19 16:43:48 +0000

    chore: working on build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -e
 
 # Build mainnet
-TACT_ENV=mainnet VITE_MAINNET=true electron-forge make --platform=darwin --arch=universal
+TACT_ENV=mainnet VITE_MAINNET=true electron-forge make "$@"
 
 # Build testnet
-TACT_ENV=testnet electron-forge make --platform=darwin --arch=universal
+TACT_ENV=testnet electron-forge make "$@"
